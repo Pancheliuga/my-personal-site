91665aeb651b41f074c7522d659bb77545093135
Pancheliuga <pan4ik@gmail.com>
2022-05-07 20:52:06 +0300

    Add flood project

--- .eleventy.js
+++ .eleventy.js
@@ -25,6 +25,7 @@ module.exports = config => {
 
     // Set directories to pass through to the dist folder
     config.addPassthroughCopy('./src/images/');
+    config.addPassthroughCopy('./src/assets/');
 
     // Only minify HTML if we are in production because it slows builds _right_ down
     if (isProduction) {
